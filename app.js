f596f28570340aaea469836696aa77c872ebcdd8
Nnanyielugo <kz.onyegbule@gmail.com>
2017-08-06 18:20:35 +0100

    removed serve

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
-app.use(favicon());
+//app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());

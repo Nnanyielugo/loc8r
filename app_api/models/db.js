63faa2c93a2ce0010dcb6f47459f6af722804ca1
Nnanyielugo <kz.onyegbule@gmail.com>
2018-01-25 12:25:19 +0100

    fixed remote database errors

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose');
 var gracefulShutdown;
 var dbURI = 'mongodb://localhost/Loc8r_new';
+var config = require('../../config/config');
 
 if(process.env.NODE_ENV === 'production') {
-    dbURI = process.env.MONGOLAB_URI;
+    dbURI = config.MONGOLAB_URI;
 }
 mongoose.connect(dbURI);
 
